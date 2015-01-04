9435de54a7e614e50a0412de2745796ff0a13f21
王健 <wangjian2254@gmail.com>
2015-01-04 09:38:29 +0800

    “题”审核功能

--- teach/urls.py
+++ teach/urls.py
@@ -5,7 +5,8 @@ Created on 2011-3-19
 @author: 王健
 '''
 from django.conf.urls import patterns
-from teach.views import add_template, template_list, save_template, save_subject, add_subject, look_view, subject_list
+from teach.views import add_template, template_list, save_template, save_subject, add_subject, look_view, subject_list, \
+    unchecker_subject_list, check_subject
 
 
 urlpatterns = patterns('^teach/',
@@ -17,6 +18,8 @@ urlpatterns = patterns('^teach/',
                        (r'^save_subject',save_subject),
                        (r'^subject_list',subject_list),
                        (r'^look_view',look_view),
+                       (r'^unchecker_subject_list',unchecker_subject_list),
+                       (r'^check_subject',check_subject),
 
 
 
--- teach/views.py
+++ teach/views.py
@@ -10,7 +10,7 @@ from teach.models import SubjectTemplate, Subject
 @login_required
 def template_list(request):
     tlist = SubjectTemplate.objects.all().order_by('-id')
-    return render_to_response('templatelist.html', {'tlist':tlist})
+    return render_to_response('templatelist.html', {'tlist':tlist, 'request': request})
 @login_required
 def add_template(request):
     id = request.REQUEST.get('id', '')
@@ -18,7 +18,7 @@ def add_template(request):
         temp = SubjectTemplate.objects.get(pk=id)
     else:
         temp = SubjectTemplate()
-    return render_to_response('templateedit.html', {'s':temp})
+    return render_to_response('templateedit.html', {'s':temp, 'request': request})
 
 @login_required
 def save_template(request):
@@ -36,7 +36,7 @@ def save_template(request):
     for s in Subject.objects.filter(template=uf.instance):
         s.is_right = None
         s.save()
-    return render_to_response('templateedit.html', {'s':uf.instance,'msg':u'保存成功'})
+    return render_to_response('templateedit.html', {'s':uf.instance,'msg':u'保存成功', 'request': request})
 
 
 @login_required
@@ -44,7 +44,7 @@ def subject_list(request):
     id = request.REQUEST.get('id')
     temp = SubjectTemplate.objects.get(pk=id)
     tlist = Subject.objects.filter(template_id=id).order_by('-id')
-    return render_to_response('subjectlist.html', {'tlist':tlist, 't':temp})
+    return render_to_response('subjectlist.html', {'tlist':tlist, 't':temp, 'request': request})
 
 @login_required
 def add_subject(request):
@@ -55,7 +55,7 @@ def add_subject(request):
         s = Subject.objects.get(pk=id)
     else:
         s = Subject()
-    return render_to_response('subjectedit.html', {'s':s, 't':temp})
+    return render_to_response('subjectedit.html', {'s':s, 't':temp, 'request': request})
 
 @login_required
 def save_subject(request):
@@ -71,11 +71,37 @@ def save_subject(request):
         uf.instance.user = request.user
     uf.instance.is_right = None
     uf.instance.save()
-    return render_to_response('subjectedit.html', {'s':uf.instance,'t':uf.instance.template,'msg':u'保存成功'})
+    return render_to_response('subjectedit.html', {'s':uf.instance,'t':uf.instance.template,'msg':u'保存成功', 'request': request})
 
 @login_required
 def look_view(request):
     sid = request.REQUEST.get('sid')
     subject = Subject.objects.get(pk=sid)
     template = subject.template
-    return render_to_response('view.html', {'subject':subject,'template':template})
+    return render_to_response('view.html', {'subject':subject,'template':template, 'request': request})
+
+
+@login_required
+def check_subject(request):
+    if not request.user.is_staff:
+        return HttpResponse(u'您不是管理员')
+    sid = request.REQUEST.get('sid')
+    do = request.REQUEST.get('do', '0')
+    subject = Subject.objects.get(pk=sid)
+    if do == '0':
+        subject.is_right = False
+    else:
+        subject.is_right = True
+    subject.checker = request.user
+    subject.save()
+
+    return HttpResponseRedirect('/teach/subject_list?id=%s'%subject.template_id)
+
+
+@login_required
+def unchecker_subject_list(request):
+
+    tlist = Subject.objects.exclude(is_right=True).order_by('id')
+    if request.REQUEST.get('user'):
+        tlist = tlist.filter(user=request.user)
+    return render_to_response('subjectlist.html', {'tlist':tlist, 'request': request})
